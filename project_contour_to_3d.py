2d9e163f5223f749bc3c65230f525028958a69d5
Павел Соликов <p.solikov@geoscan.lan>
2019-01-10 20:18:16 +0300

    Merged segmentation code into sctript

--- project_contour_to_3d.py
+++ project_contour_to_3d.py
@@ -2,11 +2,30 @@ import PhotoScan as ps
 import json
 import numpy as np
 import cv2
+from skimage.transform import AffineTransform, warp
+from skimage.color import label2rgb
 
 # Run this on //psdevscns/ps_storage/solikov/kazan_part/P084_1710_Kazan_III-IV_E-8
 
 # path to local json: C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/selected_5
 # path to local photos folder: C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/
+
+# Function to get contour of a building
+# It is required to have 3 files in provided location: image with the building, image with background markers, image with foreground markers
+# format: 'name.JPG', 'name_bg.JPG', 'name_fg.JPG'
+def get_contour(location, filename):
+    JPG = '.JPG'
+    img = cv2.imread(location + filename + JPG)
+    markers = cv2.imread(location + filename + '_fg' + JPG, 0)
+    markers_bg = cv2.imread(location + filename + '_bg' + JPG, 0)
+    labels = np.zeros_like(markers, dtype=np.int32)
+    labels[markers == 255] = 1
+    labels[markers_bg == 255] = 2
+    out = cv2.watershed(img, labels)
+    contour_image = np.zeros_like(out)
+    contour_image[out == -1] = 255
+    im2, contours, hierarchy = cv2.findContours(contour_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    return contours[0]
 
 if __name__ == "__main__":
     # Loading names of cameras containing school that I picked manuallly
