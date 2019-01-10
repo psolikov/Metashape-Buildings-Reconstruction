a95b70e2ac1c93d36da1c4f8ca86933e2efd9541
Павел Соликов <p.solikov@geoscan.lan>
2019-01-10 18:20:42 +0300

    Cleanup

--- Script.py
+++ Script.py
@@ -3,87 +3,64 @@ import json
 import numpy as np
 import cv2
 
-# path to this file local: C:\Users\p.solikov\Work\FromSegmentation\Semi-Automatic\Script.py
-# C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/Script.py
+# Run this on //psdevscns/ps_storage/solikov/kazan_part/P084_1710_Kazan_III-IV_E-8
 
-file = open('C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/selected_5')
+# path to local json: C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/selected_5
+# path to local photos folder: C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/
+
+# Loading names of cameras containing school that I picked manuallly
+file = open('//psdevscns/ps_storage/solikov/Segmentation/Chunk/selected_5')
 selected = json.load(file)
 doc = ps.app.document
 chunk = doc.chunk
 cameras = chunk.cameras
+
 selected_cameras = []
 for i in cameras:
-	if (i.label in selected):
-		selected_cameras.append(i)
+    if (i.label in selected):
+        selected_cameras.append(i)
+
+print('Loading contours...')
+
+# Loading pictures of segmented contours
 selected_contours = []
-location = 'C:/Users/p.solikov/Work/FromSegmentation/Semi-Automatic/'
-print('Loading contours')
+location = '//psdevscns/ps_storage/solikov/Segmentation/Chunk/'
 for i in selected_cameras:
-	f = location + i.label[:len(i.label) - 4] + '_contour.JPG'
-	selected_contours.append(cv2.imread(f, 0))
-ps_cam = selected_cameras[0]
-mask = ps.Mask()
-mask.setImage(ps_cam.image())
-mask_image = mask.image()
-mask_image_byte = mask_image.tostring()
-mask_image_byte = np.asarray(list(mask_image_byte), dtype='uint8')
-mask_image_byte = mask_image_byte.reshape((4000, 6000))
-# self.mask = Mask(mask_image_byte)
+    f = location + i.label[:len(i.label) - 4] + '_contour.JPG'
+    print(f)
+    selected_contours.append(cv2.imread(f, -1))
 
-def save_contour(name):
-	im2, contours, hierarchy = cv2.findContours(selected_contours[0], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-	cv2.imwrite(name, cv2.drawContours(im2, contours, -1, (0,255,0), 3))
+print('Done. Call \'print_available_cameras()\' to get list of cameras, ' +
+    '\'find_contour_3d()\' to generate vertices for new shape and '+
+    '\'create_new_shape()\' to create shape and show it.')
 
-def find_contour_3d(points):
-    # self._get_mask()
-    # contour_2d = self.mask.find_contour()
-    # contour_2d[contour_2d != 0] = 1
-    # contour_2d_dots = list(zip(*np.where(contour_2d == 1)))
-    im2, contours, hierarchy = cv2.findContours(selected_contours[0], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+def print_available_cameras():
+    for i in range(len(selected_cameras)):
+        print('id: ' + str(i) + ', name: ' + '\'' + selected_cameras[i].label + '\'')
+
+# Export 2d contour from segmented image
+def find_contour_3d(camera_index):
+    p = []
+    ps_cam = selected_cameras[camera_index]
+    im2, contours, hierarchy = cv2.findContours(selected_contours[camera_index], cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+    contour_2d = contours[0]
     point_cloud = chunk.dense_cloud
-    contour_2d = contours
-    # for (x, y) in contour_2d_dots:
-    #     pixel = contour_2d[x][y]
-    #     pixel_vector = ps.Vector([x, y])
-    #     destination_camera = ps_cam.sensor.calibration.unproject(pixel_vector)  # 3d point on ray (camera space)
-    #     destination_chunk = ps_cam.transform.mulp(destination_camera)  # 3d point on ray (chunk space)
-    #     target_point = point_cloud.pickPoint(ps_cam.center, destination_chunk)  # 3d point (chunk space)
-    #     if target_point is not None:
-    #         world = chunk.transform.matrix.mulp(target_point)  # get geocentric coords
-    #         projected = chunk.shapes.crs.project(world) #get geographic coords
-    #         # projected = np.asarray(projected)
-    #         # points.append(projected)    	
-    #         shp = chunk.shapes.addShape()
-    #         shp.vertices =  list(projected)
-
     for pixel_list in contour_2d:
         for pixel in pixel_list:
-            y, x = pixel[0]
-            pixel_vector = ps.Vector([x, y])
+            pixel_vector = ps.Vector([pixel[0], pixel[1]])
             destination_camera = ps_cam.sensor.calibration.unproject(pixel_vector)  # 3d point on ray (camera space)
             destination_chunk = ps_cam.transform.mulp(destination_camera)  # 3d point on ray (chunk space)
             target_point = point_cloud.pickPoint(ps_cam.center, destination_chunk)  # 3d point (chunk space)
             if target_point is not None:
                 world = chunk.transform.matrix.mulp(target_point)  # get geocentric coords
                 projected = chunk.shapes.crs.project(world) #get geographic coords
-                projected = np.asarray(projected)
-                points.append(projected)
-    return points
+                p.append(projected)
+    return p
 
-    # for i in range(len(contour_2d)):
-    #     for j in range(len(contour_2d[i])):
-    #         pixel = contour_2d[i][j]
-    #         if (pixel == 0):
-    #             print(pixel)
-    #             continue
-    #         print(pixel)
-    #         pixel_vector = ps.Vector([i, j])
-    #         destination_camera = ps_cam.sensor.calibration.unproject(pixel_vector)  # 3d point on ray (camera space)
-    #         destination_chunk = ps_cam.transform.mulp(destination_camera)  # 3d point on ray (chunk space)
-    #         target_point = point_cloud.pickPoint(ps_cam.center, destination_chunk)  # 3d point (chunk space)
-    #         if target_point is not None:
-    #             world = chunk.transform.matrix.mulp(target_point)  # get geocentric coords
-    #             projected = chunk.crs.project(world) #get geographic coords
-    #             projected = np.asarray(projected)
-    #             points.append(projected)
-    # return points
+# Create new shape and place it on point cloud
+def create_new_shape(label, vertices):
+    shapes = chunk.shapes
+    sh = shapes.addShape()
+    sh.label = label
+    sh.vertices = vertices
+    return sh
